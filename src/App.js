4caaf938a3925c334b7f698f9e1a312db7483d10
Kang, KugChin <kckang.dev@gmail.com>
2022-09-27 17:29:39 +0900

    Footer 컴포넌트 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import requests from './api/requests';
 import './App.css';
 import Banner from './components/Banner';
+import Footer from './components/Footer';
 import Nav from './components/Nav';
 import Row from './components/Row';
 
@@ -25,6 +26,8 @@ function App() {
       <Row title="Horror Movies" id="HM" fetchUrl={requests.fetchHorrorMovies} />
       <Row title="Romance Movies" id="RM" fetchUrl={requests.fetchRomanceMovies} />
       <Row title="Documentaties" id="DM" fetchUrl={requests.fetchDocumentaries} />
+
+      <Footer />
     </div>
   );
 }
